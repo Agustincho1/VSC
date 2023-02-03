689e32085293fb8afd940bc45ee15fc15e543efa
Agustincho1 <104277333+Agustincho1@users.noreply.github.com>
2023-02-03 12:51:32 -0300

    Delete .C directory

--- .C/Ejercicios 1/Ejercicio 6.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-/*Escribir el código necesario para mostrar los primeros 10 números primos 
-Al finalizar el listado, mostrar su suma.*/
-
-void main(){
-	
-}
--- .C/Practicas/Repaso/Ejercitacion1/Ejercicio6.c
+++
@@ -1,3 +0,0 @@
-/*Ejercicio 6)  
-Escribir el código necesario para mostrar los primeros 10 números primos 
-Al finalizar el listado, mostrar su suma.               */
